7f14098b7a36007d54059f573232e72aa4f6062a
Oliver Maketso <olivermaketso>
2024-07-26 13:21:47 +0300

    added the first flask taksks

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module starts a web app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def Hello_HBNB():
+    """returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """returns c followed by the value of text variable"""
+    text = text.replace('_',' ')
+    return "C %s" % text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module starts a web app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def Hello_HBNB():
+    """returns Hello HBNB!"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """returns c followed by the value of text variable"""
+    text = text.replace('_',' ')
+    return "C %s" % text
+
+@app.route("/python/", defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """returns c followed by the value of text variable"""
+    text = text.replace('_',' ')
+    return "python %s" % text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
