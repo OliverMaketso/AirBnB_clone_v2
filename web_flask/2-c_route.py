f56b2665d8e614b6018caea2ab5ccdc8c0121e6a
Oliver Maketso <olivermaketso>
2024-07-30 20:59:43 +0300

    Removed white spaces

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -4,20 +4,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def Hello_HBNB():
     """returns Hello HBNB!"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
     """returns c followed by the value of text variable"""
-    text = text.replace('_',' ')
+    text = text.replace('_', ' ')
     return "C %s" % text
 
 
